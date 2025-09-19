e20cf9c279c21feedb5812c489f0c3c169ff4e8d
krishnaguptha02 <krishnaguptha02@gmail.com>
2025-09-19 16:41:41 +0530

    updates

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,3 @@
-output "cluster_name" {
-  description = "Name of the GKE cluster"
-  value       = google_container_cluster.primary.name
-}
-
-output "cluster_endpoint" {
-  description = "Endpoint of the GKE cluster"
-  value       = google_container_cluster.primary.endpoint
-  sensitive   = true
-}
-
-output "cluster_ca_certificate" {
-  description = "CA certificate of the GKE cluster"
-  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-  sensitive   = true
-}
-
-output "jenkins_service_account_email" {
-  description = "Email of the Jenkins service account"
-  value       = google_service_account.jenkins_sa.email
-}
-
-output "gke_node_service_account_email" {
-  description = "Email of the GKE node service account"
-  value       = google_service_account.gke_node_sa.email
-}
-
 output "cluster_location" {
   description = "Location of the GKE cluster"
   value       = google_container_cluster.primary.location
